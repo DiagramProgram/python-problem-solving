0154a6514a91bbd9988df8e287996cdce63a8ce8
Yuktesh <yukteshm@gmail.com>
2020-04-15 17:02:05 -0400

    Create 30daysofcode-6.py

---
+++ 30daysofcode-6.py
@@ -0,0 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+
+for i in range(0, n):
+    x = input()
+    p = list(x)
+    #print(p)
+    even = ""
+    odd = ""
+    s = 0
+    while s < len(p):
+        if s % 2 == 0:
+            even += p[s]
+        else:
+            odd += p[s]
+        s+=1
+    
+    '''for each in p:
+        print(each)
+        print(p.index(each))
+        if p.index(each) % 2 == 0:
+            even += each
+        else:
+            odd += each'''
+    print(even + " " + odd)
