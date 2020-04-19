5a02c457efcd985094857b8b6c162e92e8e5d4ad
Yuktesh <yukteshm@gmail.com>
2020-04-18 22:04:24 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,29 @@
+def lists(n):
+    arry = []
+    for i in range(n):
+        x = input()
+        if "insert" in x:
+            n = x.split()
+            a = int(n[1])
+            b = int(n[2])
+            arry.insert(a, b)
+        elif "remove" in x:
+            n = x.split()
+            a = int(n[1])
+            arry.remove(a)
+        elif "append" in x:
+            n = x.split()
+            a = int(n[1])
+            arry.append(a)
+        elif x == "print":
+            print(arry)
+        elif x == "sort":
+            arry.sort()
+        elif x == "pop":
+            arry.pop()
+        elif x == "reverse":
+            arry.reverse()
+
+
+n = int(input())
+lists(n)
