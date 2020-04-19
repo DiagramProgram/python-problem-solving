37ca26f84ed3c23a10d197115995053ec3b39c4b
Yuktesh <yukteshm@gmail.com>
2020-04-18 22:03:00 -0400

    Create kattissecuredoors.py

---
+++ kattissecuredoors.py
@@ -0,0 +1,38 @@
+nom = []
+log = []
+def securedoors(n, e, name):
+    if e == "entry" and nom.count(name) == 0:
+        print(name + " entered")
+
+    elif e == "exit" and nom.count(name) == 0:
+        print(name + " exited (ANOMALY)")
+    
+    elif e == "entry" and nom.count(name) >= 1:
+        backnom = list(reversed(nom))
+        backlog = list(reversed(log))
+        num = backnom.index(name)
+        if backlog[num] == "entry":
+            print(name + " entered (ANOMALY)")
+        else:
+            print(name + " entered")
+    
+    elif e == "exit" and nom.count(name) >= 1:
+        backnom = list(reversed(nom))
+        backlog = list(reversed(log))
+        num = backnom.index(name)
+        if backlog[num] == "exit":
+            print(name + " exited (ANOMALY)")
+        else:
+            print(name + " exited")
+    
+    nom.append(name)
+    log.append(e)
+
+
+n = int(input())
+
+for each in range(n):
+    e, name = input().split()
+    securedoors(n, e, name)
+    
+    
